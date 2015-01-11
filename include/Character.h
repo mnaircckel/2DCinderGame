ba29d03f68a082907ba2e68a8d13463d7d3387e1
Nicolas Knoebber <knoebber@gmail.com>
2015-01-11 02:09:02 -0800

    Added sprite field

--- include/Character.h
+++ include/Character.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "cinder/gl/gl.h"
 #include "Camera2D.h"
+#include "cinder/gl/texture.h"
 using namespace ci;
 using namespace std;
 
@@ -8,6 +9,7 @@ class Character
 {
 public:
 	Character();
+	gl::Texture sprite;
 	Character(Vec2f newLocation, Vec2f newVelocity, Vec2f newAcceleration);
 
 	Vec2f location;
